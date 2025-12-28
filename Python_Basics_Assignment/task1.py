9aff0558705f952feb8371f4e33c4b919f3719bc
Nikhil-251997 <gupta251997@gmail.com>
2025-12-28 21:24:59 +0530

    Organized tasks into folders and renamed files

---
+++ Python_Basics_Assignment/task1.py
@@ -0,0 +1,14 @@
+#Task 1
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+substraction = num2 - num1
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Addition:" , num1 + num2)
+print("Substraction:" , num1 - num2)
+print("Multiplication:" , num1 * num2)
+print("Division:" , num1 / num2)
